1e1e97f0e015a0417fa18a0f4123578e9406db2f
Chonjou Chiang <chonjou@gmail.com>
2018-02-08 09:47:30 -0800

    Add project files.

---
+++ LMYCWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMYCWebsite.Startup))]
+namespace LMYCWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
