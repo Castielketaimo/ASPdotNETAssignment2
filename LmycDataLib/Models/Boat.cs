fe2b68cd0947bee01f1ef3939aeb162146b0ca60
Castiel Li <castiel.andrew@gmail.com>
2018-02-15 13:10:22 -0800

    Added Boat Models

--- LmycDataLib/Models/Boat.cs
+++ LmycDataLib/Models/Boat.cs
@@ -1,6 +1,7 @@
 ﻿using LMYCWebsite.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -13,19 +14,35 @@ namespace LmycDataLib.Models
     {
         [key]
         public string BoatId { get; set; }
+        [Required(ErrorMessage = "Please Enter Boat Name")]
         [StringLength(50, ErrorMessage = "BoatName cannot be longer than 50 characters.")]
+        [DisplayName("Boat Name")]
         public string BoatName { get; set; }
+
+        [DisplayName("Image Url")]
+        [Required(ErrorMessage = "Please Enter Image Url")]
         public string Picture { get; set; }
+
+        [DisplayName("Length In Feet")]
+        [Required(ErrorMessage = "Please Enter Length In Feet")]
         public int LengthInFeet { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Make of the boat")]
         public string Make { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Year of the boat being made")]
+        [Range(1900, 2017, ErrorMessage = "Year Should Be Between 1900 and 2017")]
         public int Year { get; set; }
+
+        [ScaffoldColumn(false)]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime RecordCreationDate { get; set; }
 
-        [ForeignKey("UserId")]
+        [ScaffoldColumn(false)]
+        [ForeignKey("User")]
         [Display(Name = "Created By")]
-        [Column("CreatedBy")]
+        public string CreatedBy { get; set; }
         public virtual ApplicationUser User { get; set; }
 
 
